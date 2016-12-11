b5c4070b1cf9413dbf0df72cf401e7fe13021e35
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-12-11 13:39:25 -0200

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const gulp = require('gulp');
 const del = require('del');
 const cleanCSS = require('gulp-clean-css');
@@ -8,50 +6,50 @@ const uglify = require('gulp-uglify');
 const image = require('gulp-image');
 const webserver = require('gulp-webserver');
 
-let paths = {
+const paths = {
   dist: 'dist',
   images: 'assets/images',
   stylesheets: 'assets/stylesheets',
   javascripts: 'assets/javascripts',
-  templates: ['_includes/**', '_layouts/**', '_mixins/**', '_site/**']
+  templates: ['_includes/**', '_layouts/**', '_mixins/**', '_site/**'],
 };
 
 gulp.task('clean:images', () => {
-  return del(`${paths.dist}/${paths.images}`);
+  del(`${paths.dist}/${paths.images}`);
 });
 
 gulp.task('clean:stylesheets', () => {
-  return del(`${paths.dist}/${paths.stylesheets}`);
+  del(`${paths.dist}/${paths.stylesheets}`);
 });
 
 gulp.task('clean:javascripts', () => {
-  return del(`${paths.dist}/${paths.javascripts}`);
+  del(`${paths.dist}/${paths.javascripts}`);
 });
 
 gulp.task('clean:templates', () => {
-  return del(`${paths.dist}/**.html`);
+  del(`${paths.dist}/**.html`);
 });
 
 gulp.task('images', ['clean:images'], () => {
-  return gulp.src(`${paths.images}/**`)
+  gulp.src(`${paths.images}/**`)
     .pipe(image())
-    .pipe(gulp.dest(`${paths.dist}/${paths.images}`))
+    .pipe(gulp.dest(`${paths.dist}/${paths.images}`));
 });
 
 gulp.task('minify', ['clean:stylesheets'], () => {
-  return gulp.src(`${paths.stylesheets}/**.css`)
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+  gulp.src(`${paths.stylesheets}/**.css`)
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest(`${paths.dist}/${paths.stylesheets}`));
 });
 
 gulp.task('templates', ['clean:templates'], () => {
   gulp.src(paths.templates[3])
     .pipe(jade())
-    .pipe(gulp.dest(paths.dist))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('uglify', ['clean:javascripts'], () => {
-  return gulp.src(`${paths.javascripts}/**.js`)
+  gulp.src(`${paths.javascripts}/**.js`)
     .pipe(uglify())
     .pipe(gulp.dest(`${paths.dist}/${paths.javascripts}`));
 });
@@ -65,12 +63,12 @@ gulp.task('watch', () => {
 });
 
 gulp.task('serve', () => {
-  return gulp.src('./dist')
+  gulp.src('./dist')
     .pipe(webserver({
       directoryListing: false,
       open: true,
       port: process.env.PORT || '8080',
-      host: process.env.IP || 'localhost'
+      host: process.env.IP || 'localhost',
     }));
 });
 
