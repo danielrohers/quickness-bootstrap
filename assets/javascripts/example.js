4a530e17c744c0d49719be79eaeb11f32b1256b5
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-02-07 17:44:25 -0200

    improved example.js

--- assets/javascripts/example.js
+++ assets/javascripts/example.js
@@ -4,34 +4,15 @@
 
   var Main = (function () {
 
-    var _getPathname = function () {
-      return window.location.pathname;
-    };
-
-    var _isMenuActive = function (path) {
-      return path === _getPathname();
-    };
-
-    var _setMenuActive = function () {
-      var links = document.querySelectorAll('nav a');
-      [].forEach.call(links, function (link) {
-        if (_isMenuActive(link.getAttribute('href'))) {
-          link.parentElement.classList.add('active');
-        }
-      });
-    }
-
     return {
 
       init : function () {
-        _setMenuActive();
+        console.log('quickness');
       }
 
     }
 
   })();
-
-  window.Main = Main;
 
   Main.init();
 
